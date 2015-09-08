d968026f2bd1b42ecc4a35c16c673c90180a430d
Jonathan DeFaveri <d.jonathan@gmail.com>
2015-09-07 21:30:51 -0400

    animations added

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 
 $(document).ready( function() {
 
-$('.project').hover(function() {
-    $(this).css("cursor", "pointer");
-    $(this).animate({
-        width: "50%",
-        height: "50%"
-    }, 'slow');
+	$('.introduction-expand').hover(function() {
+		$('.introduction-content').slideDown(400);
+		$('.introduction-expand').hide()
+	});
 
-}
+	$(window).scroll(function() {
+		$('.introduction-content').slideDown(400);
+		$('.introduction-expand').hide()
+
+	});
+
+
+	$('.project img').mouseenter(function(e) {
+		var box = $(e.target)
+	    box.css("cursor", "pointer");
+	    box.animate({
+	        width: "105%",
+	        height: "130%"
+	    }, 'slow');
+
+	  });
+
+	$('.project img').mouseleave(function(e) {
+		var box = $(e.target)
+	    box.animate({
+	        width: "100%",
+	        height: "100%"
+	    }, 'slow');
+	});
 
 });
